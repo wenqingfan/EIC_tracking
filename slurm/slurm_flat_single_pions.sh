75aedc826c27ad44e66cecc8603c3bd6b634814b
wfan <wenqing.fan@cern.ch>
2022-10-07 12:24:55 -0700

    add some extra comments

--- slurm/slurm_flat_single_pions.sh
+++ slurm/slurm_flat_single_pions.sh
@@ -11,7 +11,7 @@
 #SBATCH --mem=16GB
 
 echo running job script with task ID $SLURM_ARRAY_TASK_ID
-GEN_P_RANGE=0 # 0: low p, 1: mid p, 2: high p, 3: extra high p
-GEN_ETA_RANGE=2
+GEN_P_RANGE=2 # 0: low p, 1: mid p, 2: high p, 3: extra high p
+GEN_ETA_RANGE=0 # 0: -3.5 to 3.5, 1: 3.5 to 4, 2: -4 to -3.5 
 srun shifter bash run_flat_single_pions_sim.sh $SLURM_ARRAY_TASK_ID $GEN_P_RANGE $GEN_ETA_RANGE
 
